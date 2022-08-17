f793a54f3cd810b4fad56900181644e2252c56e4
Saif samer <91379641+Lord-Biko-1@users.noreply.github.com>
2022-08-17 22:05:29 +0200

    transfereListView

---
+++ app/src/main/java/com/example/matchesproject/Transfere_ListView.java
@@ -0,0 +1,14 @@
+package com.example.matchesproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Transfere_ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfere_list_view);
+    }
+}
