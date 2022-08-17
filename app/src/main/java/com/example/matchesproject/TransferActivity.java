047fe398f73628b8b9e81f517013fa654900d888
ATOM6621 <75903178+ATOM6621@users.noreply.github.com>
2022-08-17 16:23:58 +0200

    transfere view
    ana 3mlt el activity bs 3tlan fe error fe el a5er

---
+++ app/src/main/java/com/example/matchesproject/TransferActivity.java
@@ -0,0 +1,28 @@
+package com.example.matchesproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class TransferActivity extends AppCompatActivity {
+ListView transferList;
+ArrayAdapter<String> transferAdapter;
+TransferDataBase transfer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer);
+
+        transferList = (ListView) findViewById(R.id.list_of_transfers);
+        transferAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1);
+        transferList.setAdapter(transferAdapter);
+
+        transfer = new TransferDataBase(getApplicationContext());
+        ArrayAdapter<Transfere> adapter = transfer.getTransfers();
+
+    }
+}
